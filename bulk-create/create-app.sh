381af38c2a95e594d18ee79a98a1c2acc7de2bd0
Jesse Kennedy <jessek@redhat.com>
2015-07-01 15:00:28 -0400

    stuff

--- bulk-create/create-app.sh
+++ bulk-create/create-app.sh
@@ -9,6 +9,6 @@
 
 if [ \! -d /var/lib/openshift/${2}-${1} ]; then
   htpasswd -b /etc/openshift/htpasswd "$1" redhat
-  curl -X POST -k -u $1:redhat -H "Content-Type: application/json" -d '{ "name": "'$2'", "cartridges": ["diy-0.1"], "domain_id": "'$1'", "application": { "name": "'$2'" } }' https://broker.hosts.example.com/broker/rest/applications
+  curl -X POST -k -u $1:redhat -H "Content-Type: application/json" -d '{ "name": "'$2'", "cartridges": ["'$3'"], "domain_id": "'$1'", "application": { "name": "'$2'" } }' https://broker.hosts.example.com/broker/rest/applications
   #curl -X POST -k -u $1:pass -H "Content-Type: application/json" -d '{ "event": "stop", "domain_id": "'$1'", "application_id": "'$2'", "app_event": { "event": "stop" } }' https://localhost/broker/rest/application/$2/events
 fi
