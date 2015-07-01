02f532c83bddb05a192631c9c52ffb903284753a
Andy Grimm <agrimm@redhat.com>
2015-07-01 13:02:25 -0400

    bulk app creation scripts

---
+++ bulk-create/create-app.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#
+# Create one app and stop it
+#
+# assumes domain namespace == username
+# also assumes friendly hostname symlinks in /var/lib/openshift (i.e., devenv mode)
+#
+
+if [ \! -d /var/lib/openshift/${2}-${1} ]; then
+  curl -X POST -k -u $1:pass -H "Content-Type: application/json" -d '{ "name": "'$2'", "cartridges": ["diy-0.1"], "domain_id": "'$1'", "application": { "name": "'$2'" } }' https://localhost/broker/rest/applications
+  curl -X POST -k -u $1:pass -H "Content-Type: application/json" -d '{ "event": "stop", "domain_id": "'$1'", "application_id": "'$2'", "app_event": { "event": "stop" } }' https://localhost/broker/rest/application/$2/events
+fi
